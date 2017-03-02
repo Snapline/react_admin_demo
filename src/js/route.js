6b0928a8726542e5bc15a7949b7416d844b80508
gaoyang <gaoyang@think-land.com>
2017-03-02 11:20:17 +0800

    pc和移动端页头页脚完成

--- src/js/route.js
+++ src/js/route.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Router, Route, hashHistory } from 'react-router'
+import PCIndex from './components/pc_index'
+import MobileIndex from './components/mobile_index'
+import 'antd/dist/antd.css'
+
+import MediaQuery from 'react-responsive'
 
 export default class Root extends React.Component{
 	render(){
 		return(
-			<div>init</div>
+			<div>
+				<MediaQuery query='(min-device-width: 1224px)'>
+					<PCIndex/>
+				</MediaQuery>
+		 		<MediaQuery query='(max-device-width: 1224px)'>
+					<MobileIndex/>
+				</MediaQuery>
+			</div>
 		)
 	}
 }
@@ -13,5 +25,5 @@ export default class Root extends React.Component{
 
 ReactDOM.render(
 	<Root/>,
-	document.getElementById('example')
+	document.getElementById('mainContainer')
 )
